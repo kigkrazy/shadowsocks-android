18216bcf4684f5548371f5493899029b81b0098c
kigkrazy <kigkrazy@163.com>
2018-08-15 17:19:23 +0800

    1. 精简代码

--- mobile/src/main/java/com/github/shadowsocks/reizx/broadcast/RemoteControlReceiver.kt
+++ mobile/src/main/java/com/github/shadowsocks/reizx/broadcast/RemoteControlReceiver.kt
@@ -35,21 +35,22 @@ class RemoteControlReceiver : BroadcastReceiver() {
      */
     fun doAdd(context: Context, intent: Intent) {
         RssLog.d("doAdd...")
+        deleteExistConfig(intent.getStringExtra(ReizxConstants.PROXY_HOST))//删除已经存在的IP
         var profileString = ResourceUtils.readAssets2String("reizx/default_profile")
         val profile = GsonUtil.fromJsonString<Profile>(profileString, Profile::class.javaObjectType)
-
         profile.host = intent.getStringExtra(ReizxConstants.PROXY_HOST)
         profile.remotePort = intent.getIntExtra(ReizxConstants.PROXY_PORT, 8388)
         profile.method = intent.getStringExtra(ReizxConstants.PROXY_METHOD)
+        deleteExistConfig(profile.host)//删除多余的配置
         ProfileManager.updateProfile(profile)
         RssLog.d("add the profile : %s".format(GsonUtil.toJsonString(profile)))
     }
 
     fun doSwitch(context: Context, intent: Intent) {
         RssLog.d("doSwitch...")
-        var profileId = intent.getIntExtra(ReizxConstants.PROFILE_ID, -1)
-        RssLog.d("set switch : %d".format(profileId))
-        app.switchProfile(profileId)
+        var host: String = intent.getStringExtra(ReizxConstants.PROXY_HOST) ?: return
+        var profile: Profile = findProfile(host) ?: return
+        app.switchProfile(profile.id)
     }
 
     /**
@@ -73,9 +74,9 @@ class RemoteControlReceiver : BroadcastReceiver() {
      */
     fun doDelete(context: Context, intent: Intent) {
         RssLog.d("doDelete...")
-        var profileId = intent.getIntExtra(ReizxConstants.PROFILE_ID, -1)
-        ProfileManager.delProfile(profileId)
-        RssLog.d("delete switch : %d".format(profileId))
+        var host = intent.getStringExtra(ReizxConstants.PROXY_HOST)
+        RssLog.d("doDelete the config : $host")
+        deleteExistConfig(host)
     }
 
     /**
@@ -83,9 +84,30 @@ class RemoteControlReceiver : BroadcastReceiver() {
      */
     fun doDeleteAll(context: Context, intent: Intent) {
         RssLog.d("doDeleteAll...")
-        var profiles = ProfileManager.getAllProfiles()
-        for (profile in profiles!!) {
+        var profiles = ProfileManager.getAllProfiles()?.toMutableList() ?: mutableListOf()
+        for (profile in profiles) {
             ProfileManager.delProfile(profile.id)
         }
+    }
+
+    /**
+     * 删除存在相同IP的配置
+     */
+    private fun deleteExistConfig(host: String) {
+        var profiles = ProfileManager.getAllProfiles()?.toMutableList() ?: mutableListOf()
+        for (profile in profiles) {
+            if (profile.host == host) ProfileManager.delProfile(profile.id)
+        }
+    }
+
+    /**
+     * 删除存在相同IP的配置
+     */
+    private fun findProfile(host: String): Profile? {
+        var profiles = ProfileManager.getAllProfiles()?.toMutableList() ?: mutableListOf()
+        for (profile in profiles) if (profile.host == host) {
+            return profile
+        }
+        return null
     }
 }
--- mobile/src/main/java/com/github/shadowsocks/reizx/constant/ReizxConstants.kt
+++ mobile/src/main/java/com/github/shadowsocks/reizx/constant/ReizxConstants.kt
@@ -2,9 +2,9 @@ package com.github.shadowsocks.reizx.constant
 
 class ReizxConstants {
     companion object {
-        val PROFILE_ID : String = "profile_id"
-        val PROXY_HOST : String = "proxy_host"
-        val PROXY_PORT : String = "proxy_port"
-        val PROXY_METHOD : String = "proxy_method"
+        const val PROFILE_ID : String = "profile_id"
+        const val PROXY_HOST : String = "proxy_host"
+        const val PROXY_PORT : String = "proxy_port"
+        const val PROXY_METHOD : String = "proxy_method"
     }
 }
