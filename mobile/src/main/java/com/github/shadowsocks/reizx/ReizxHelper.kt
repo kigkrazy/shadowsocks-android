8eabb70f9f79a9542320079600cf1fe7058f5367
kigkrazy <kigkrazy@163.com>
2018-09-07 23:19:55 +0800

    配置文件读取错误时候的排错工作

--- mobile/src/main/java/com/github/shadowsocks/reizx/ReizxHelper.kt
+++ mobile/src/main/java/com/github/shadowsocks/reizx/ReizxHelper.kt
@@ -13,20 +13,23 @@ object ReizxHelper {
     private lateinit var app: App;
 
     fun setReizxConfig(app: App) {
-        ReizxHelper.app = app
+        try {
+            ReizxHelper.app = app
 
-        //文件不存在直接退出reizx的配置
-        var configFile = File(PROXY_CONFIG_PATH)
-        if (!configFile.exists()) return
-
-        RssLog.d("find the reizx config, there will reset profile.")
-        RssLog.d("clear profile")
-        cleanProfile()
-        RssLog.d("reset the profile...")
-        var profileString = FileUtils.readFileToString(configFile, "utf-8")
-        var profile: Profile = GsonUtil.fromJsonString(profileString, Profile::class.javaObjectType)
-        RssLog.d("set the profile : $profileString")
-        ProfileManager.updateProfile(profile)
+            //文件不存在直接退出reizx的配置
+            var configFile = File(PROXY_CONFIG_PATH)
+            if (!configFile.exists()) return
+            RssLog.d("find the reizx config, there will reset profile.")
+            var profileString = FileUtils.readFileToString(configFile, "utf-8")
+            var profile: Profile = GsonUtil.fromJsonString(profileString, Profile::class.javaObjectType)
+            RssLog.d("reset the profile...")
+            RssLog.d("clear profile")
+            cleanProfile()
+            RssLog.d("set the profile : $profileString")
+            ProfileManager.updateProfile(profile)
+        }catch (e: Exception){
+            RssLog.e("init reizx config error : $e")
+        }
     }
 
 
